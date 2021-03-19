3b1a22e429d3d0fa5cd06f48bc33e941ad75737a
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-03-18 20:51:29 -0600

    updating

--- API com Express/controllers/atendimento.js
+++ API com Express/controllers/atendimento.js
@@ -1,5 +1,8 @@
 module.exports = app => {
     app.get('/atendimento', (req, res) => res.send('Servidor rodando, tudo ok, você está realizando um GET'))
 
-    app.post('/atendimento', (req, res) => res.send('voce esta realizando um POST'))
+    app.post('/atendimento', (req, res) => {
+        console.log(req.body)
+        res.send('voce esta realizando um POST')
+    })
 }
