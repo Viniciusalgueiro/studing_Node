f15ea65ba2907c066443a79cbf5f109a495bfd54
Vinicius Salgueiro Costa <68074908+Viniciusalgueiro@users.noreply.github.com>
2022-09-17 02:51:05 -0300

    controller

--- Alura_Project/src/router/livrosRouter.js
+++ Alura_Project/src/router/livrosRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
+import LivroController from "../Controller/livrosController.js";
 
 import LivrosController from "../Controller/livrosController.js";
 
 const router = express.Router();
 
 router
-    .get ('/livros', LivrosController.listarlivros)
+    .get ("/livros", LivrosController.listarlivros)
+    .post("/livros", LivrosController.cadastrarLivro)
 
 export default router
